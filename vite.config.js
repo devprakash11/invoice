9a6f97d4b5b210da8a9c65d77943858f2e199fd7
Dev Prakash <141032391+devprakash11@users.noreply.github.com>
2026-08-14 23:36:34 +0530

    add file netlify TOML and update package and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-})
+
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+
+  server: {
+    port: 5173,
+    host: true,
+  },
+
+  preview: {
+    port: 4173,
+    host: true,
+  },
+});
